60c0f98b9c2b934ab8e9a573b5532905919afcc6
changkong2046 <1324227141@qq.com>
2015-05-10 20:41:05 +0800

    第一次初始化

---
+++ weibo/weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weibo
+//
+//  Created by 胡雅敏 on 15/5/9.
+//  Copyright (c) 2015年 胡雅敏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
